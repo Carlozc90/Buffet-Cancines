fce993e8920c5b61ec1dcde69b345563b1a46a5e
Carlos Cancines <Carlozc90@gmail.com>
2022-03-28 15:32:19 -0500

    Finalizado

--- context/BuffetProvider.jsx
+++ context/BuffetProvider.jsx
@@ -11,14 +11,14 @@ const lomasPedido = [
     imagen: "5chichapescado",
     descripcion:
       "Trozos de filete de pescado frito, acompañado de yuca frita y salsa criolla.",
-    categoriaId: 5,
+    categoriaId: "5",
   },
   {
     nombre: "Alitas BBQ",
     precio: 24.9,
     imagen: "2alitasbbq",
     descripcion: "Alitas bañadas de salsa barbebue.",
-    categoriaId: 2,
+    categoriaId: "2",
   },
   {
     nombre: "COMBINE 2 PLATOS A SU ELECCIÓN",
@@ -26,14 +26,14 @@ const lomasPedido = [
     imagen: "4combine2platos",
     descripcion:
       "Cerdo con frutas ó  pollo chijaukay ó pollo al sillau ó pollo con frutas ó pollo con verduras ó pollo tipakay + chaufa de pollo ó aeropuerto ó tallarín saltado.",
-    categoriaId: 4,
+    categoriaId: "4",
   },
   {
     nombre: "LASAGNA DE JAMÓN Y QUESO",
     precio: 19.9,
     imagen: "6lasagnaqueso",
     descripcion: "Lasagna de jamón y queso con salsa blanca y queso gratinado.",
-    categoriaId: 6,
+    categoriaId: "6",
   },
 ];
 
